bd3490cb84b82dc7fadebf0e0c9cc95ce9410d57
devbuilt888 <65559978+devbuilt888@users.noreply.github.com>
2020-05-18 13:38:36 -0400

    Add files via upload

---
+++ eventflix/envent/carousel2.php
@@ -0,0 +1,163 @@
+
+<link rel="stylesheet" href="cssnetflix.css">
+
+<div class="peliculas-recomendadas contenedor">
+    <div class="contenedor-titulo-controles">
+     <a href=""><h2>Planes de Verano &nbsp;<i class="fas fa-angle-right"></i></h2></a> 
+      <div class="indicadores"></div>
+    </div>
+  
+    <div class="contenedor-principal">
+      <button role="button" id="flecha-izquierda" class="flecha-izquierda"><i class="fas fa-angle-left"></i></button>
+  
+      <div class="contenedor-carousel">
+        <div class="carousel">
+          <div class="pelicula">
+            <a href="m.html">
+                    <img src="img/netflix1.jpeg" alt="">
+                <div class="datos-contenido">
+                    <div class="title-content">
+                      <h4 class="title-datos">Hola como estassdakdjansdjkdksnadajkdnsajndlnklnjkalnsfkdajsnfjkadsnf;akjnfkjadnfjlk</h4>
+                      <div class="cont-data">
+                        <fa-icon class="ng-fa-icon"><i class="fas fa-star"></i></fa-icon> 4.6
+                        <span>/5</span>
+                      </div>
+                      <div class="cont-price">
+                        <div class="plan-price">
+                          <span class="plan-price-text">Desde</span>
+                          <span class="plan-price-num">10.50$</span>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+            </a>
+          </div>
+          <div class="pelicula">
+              <a href="m.html">
+                      <img src="img/netflix2.jpg" alt="">
+                  <div class="datos-contenido">
+                      <div class="title-content">
+                        <h4 class="title-datos">Hola como estassdakdjansdjkdksnadajkdnsajndlnklnjkalnsfkdajsnfjkadsnf;akjnfkjadnfjlk</h4>
+                        <div class="cont-data">
+                          <fa-icon class="ng-fa-icon"><i class="fas fa-star"></i></fa-icon> 4.6
+                          <span>/5</span>
+                        </div>
+                        <div class="cont-price">
+                          <div class="plan-price">
+                            <span class="plan-price-text">Desde</span>
+                            <span class="plan-price-num">10.50$</span>
+                          </div>
+                        </div>
+                      </div>
+                  </div>
+            </a>
+          </div>
+          <div class="pelicula">
+            <a href="m.html">
+                    <img src="img/netflix3.jpg" alt="">
+                <div class="datos-contenido">
+                    <div class="title-content">
+                      <h4 class="title-datos">Hola como estassdakdjansdjkdksnadajkdnsajndlnklnjkalnsfkdajsnfjkadsnf;akjnfkjadnfjlk</h4>
+                      <div class="cont-data">
+                        <fa-icon class="ng-fa-icon"><i class="fas fa-star"></i></fa-icon> 4.6
+                        <span>/5</span>
+                      </div>
+                      <div class="cont-price">
+                        <div class="plan-price">
+                          <span class="plan-price-text">Desde</span>
+                          <span class="plan-price-num">10.50$</span>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+          </a>
+        </div>
+        <div class="pelicula">
+          <a href="m.html">
+                  <img src="img/netflix4.jpg" alt="">
+              <div class="datos-contenido">
+                  <div class="title-content">
+                    <h4 class="title-datos">Hola como estassdakdjansdjkdksnadajkdnsajndlnklnjkalnsfkdajsnfjkadsnf;akjnfkjadnfjlk</h4>
+                    <div class="cont-data">
+                      <fa-icon class="ng-fa-icon"><i class="fas fa-star"></i></fa-icon> 4.6
+                      <span>/5</span>
+                    </div>
+                    <div class="cont-price">
+                      <div class="plan-price">
+                        <span class="plan-price-text">Desde</span>
+                        <span class="plan-price-num">10.50$</span>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+          </a>
+        </div>
+        <div class="pelicula">
+          <a href="m.html">
+                  <img src="img/netflix5.jpeg" alt="">
+              <div class="datos-contenido">
+                  <div class="title-content">
+                    <h4 class="title-datos">Hola como estassdakdjansdjkdksnadajkdnsajndlnklnjkalnsfkdajsnfjkadsnf;akjnfkjadnfjlk</h4>
+                    <div class="cont-data">
+                      <fa-icon class="ng-fa-icon"><i class="fas fa-star"></i></fa-icon> 4.6
+                      <span>/5</span>
+                    </div>
+                    <div class="cont-price">
+                      <div class="plan-price">
+                        <span class="plan-price-text">Desde</span>
+                        <span class="plan-price-num">10.50$</span>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+          </a>
+        </div>
+        <div class="pelicula">
+          <a href="m.html">
+                  <img src="img/netflix1.jpeg" alt="">
+              <div class="datos-contenido">
+                  <div class="title-content">
+                    <h4 class="title-datos">Hola como estassdakdjansdjkdksnadajkdnsajndlnklnjkalnsfkdajsnfjkadsnf;akjnfkjadnfjlk</h4>
+                    <div class="cont-data">
+                      <fa-icon class="ng-fa-icon"><i class="fas fa-star"></i></fa-icon> 4.6
+                      <span>/5</span>
+                    </div>
+                    <div class="cont-price">
+                      <div class="plan-price">
+                        <span class="plan-price-text">Desde</span>
+                        <span class="plan-price-num">10.50$</span>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+          </a>
+        </div>
+        <div class="pelicula">
+          <a href="m.html">
+                  <img src="img/netflix1.jpeg" alt="">
+              <div class="datos-contenido">
+                  <div class="title-content">
+                    <h4 class="title-datos">Hola como estassdakdjansdjkdksnadajkdnsajndlnklnjkalnsfkdajsnfjkadsnf;akjnfkjadnfjlk</h4>
+                    <div class="cont-data">
+                      <fa-icon class="ng-fa-icon"><i class="fas fa-star"></i></fa-icon> 4.6
+                      <span>/5</span>
+                    </div>
+                    <div class="cont-price">
+                      <div class="plan-price">
+                        <span class="plan-price-text">Desde</span>
+                        <span class="plan-price-num">10.50$</span>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+          </a>
+        </div>
+        </div>
+      </div>
+  
+      <button role="button" id="flecha-derecha" class="flecha-derecha"><i class="fas fa-angle-right"></i></button>
+    </div>
+  </div>
+
+
+  
